881e41ff2343de54608e5218bb5d29abccd8b8be
Greg Blike <greg.blike@inkanitolabs.com>
2019-03-19 19:29:43 -0400

    Update to better handle serial buffer

--- ez_motor_driver/scripts/ez_motor_driver.py
+++ ez_motor_driver/scripts/ez_motor_driver.py
@@ -3,7 +3,9 @@
 import rospy
 import time
 import serial
-import threading
+import sys
+import Queue as Q
+from multiprocessing import Process, Queue
 import io
 import tf
 from geometry_msgs.msg import Twist, PoseWithCovarianceStamped
@@ -12,11 +14,14 @@ class Connection():
     def __init__(self):
         self.actions = []
         self.ser = serial.Serial('/dev/ttyACM0', 9600)
+        self.message_queue = Queue(maxsize=1)
+        if(not self.ser.isOpen()):
+            self.ser.open()
         time.sleep(2)
 
     def update_velocity(self, velocity):
         print(str(velocity))
-        directions = ('STOP', 'STOP')
+        directions = ('FORWARD', 'STOP')
         if (velocity.linear.x > 0 and velocity.angular.z == 0):
             directions = ('FORWARD', 'FORWARD')
         elif (velocity.linear.x < 0 and velocity.angular.z == 0):
@@ -26,36 +31,18 @@ class Connection():
         elif (velocity.linear.x == 0 and velocity.angular.z < 0): # Turn LEFT
             directions = ('BACKWARD', 'FORWARD')
 
-        
-        self.ser.write(self.write_message('LEFT',  directions[0], 76))
-        self.ser.write(self.write_message('RIGHT', directions[1], 76))
-
+        try:
+            self.message_queue.put(zip(["LEFT","RIGHT"], directions), False)
+        except Q.Full:
+            pass
 
     def pose_callback(self, pose):
         print(str(pose))
         #serial.write('a')
         #serial.flush(
-        
+
     def connect(self):
         pass
-
-    def write_message(self,motor, direction, speed):
-        out = bytearray()
-        if motor == 'RIGHT':
-            out.append('R')
-        else:
-            out.append('L')
-
-        if direction == 'FORWARD':
-            out.append('F')
-        elif direction == 'BACKWARD':
-            out.append('B')
-        else:
-            out.append('S')
-
-        out.append(speed)
-
-        return out
 
     def run(self):
         rospy.init_node('ezmotor', anonymous=True)
@@ -65,36 +52,54 @@ class Connection():
         self.sub = rospy.Subscriber(cmd_vel_topic,  Twist, self.update_velocity)
 
         # place to receive feedback
-        odometry_topic = '/rtabmap/localization_pose'
+        #odometry_topic = '/rtabmap/localization_pose'
         #self.sub = rospy.Subscriber(odometry_topic, PoseWithCovarianceStamped, self.pose_callback)
-        #listener = tf.TransformListener()
-        refresh = rospy.Rate(1)
+        listener = tf.TransformListener()
 
         print("Initializing")
         start = 0
-        while not rospy.is_shutdown():
-            time.sleep(0.1)
-            continue
-            try:
-                #(trans, rot) = listener.lookupTransform('/map', 'camera_link', rospy.Time(0))
-                #print(str((trans,rot)))
-                currenttime = int(round(time.time() * 1000))
-                m = self.write_message('LEFT', 'FORWARD', start)
-                ser.write(m)
-                m = self.write_message('RIGHT', 'BACKWARD', 255)
-                ser.write(m)
+        def read_input(serial_stream):
+            while not rospy.is_shutdown():
                 time.sleep(0.1)
-                if ser.inWaiting() > 0:
-                    data_str = ser.read(ser.inWaiting())
-                    print(data_str)
-                refresh.sleep()
-            except Exception, e: #(tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                print(e)
-                raise SystemExit
+                if serial_stream.inWaiting() > 0:
+                    data_str = serial_stream.read(serial_stream.inWaiting())
+                    sys.stdout.write(data_str)
+
+        def stream_commands(message_queue, serial_stream):
+            while not rospy.is_shutdown():
+                time.sleep(0.25)
+                if message_queue.qsize() > 0:
+                    message = message_queue.get()
+                    for motor, direction in message:
+                        cmd = " ".join([motor, direction, str(76)]) + "\n"
+                        print(cmd)
+                        if(self.ser.isOpen()):
+                            self.ser.write(cmd)
+                            self.ser.flush()
+                        else:
+                            print("serial connection is closed")
+
+        def ros_status():
+            while not rospy.is_shutdown():
+                time.sleep(0.1)
+
+
+        read_loop = Process(target=read_input, args=(self.ser,))
+        read_loop.daemon = True
+
+        write_loop = Process(target=stream_commands, args=(self.message_queue, self.ser,))
+        write_loop.daemon = True
+
+        ros_client = Process(target=ros_status)
+        ros_client.daemon = True
+
+        ros_client.start()
+        #read_loop.start()
+        write_loop.start()
+        ros_client.join()
+
 
 if __name__ == '__main__':
-    try:
-        serial_proxy = Connection()
-        serial_proxy.connect()
-        serial_proxy.run()
-    except rospy.ROSInterruptException: pass
+    serial_proxy = Connection()
+    serial_proxy.connect()
+    serial_proxy.run()
--- ez_motor_driver/sketch/ez_motor_drive/ez_motor_drive.ino
+++ ez_motor_driver/sketch/ez_motor_drive/ez_motor_drive.ino
@@ -1,4 +1,3 @@
-
 int STATUSLED = 13;
 
 enum Direction { STOP = 0, FORWARD = 1, BACKWARD = 2};
@@ -108,41 +107,84 @@ void setup() {
 
 unsigned long lastCommandTime = 1;
 unsigned long lastIdle = 0;
+const byte NUM_PARAMS = 3;
+
+Direction readDirection(String direction) {
+    if(direction.equalsIgnoreCase("STOP")) {
+      return STOP;
+    } else if(direction.equalsIgnoreCase("FORWARD")) {
+      return FORWARD;
+    } else if(direction.equalsIgnoreCase("BACKWARD")) {
+      return BACKWARD;
+    } else {
+      return STOP;
+    }
+}
+
+Motor* readMotor(String motorId) {
+    if(motorId.equalsIgnoreCase("Right")) {
+      return rightMotor;
+    } else {
+      return leftMotor;
+    }
+}
+
+byte readSpeed(String rawSpeed) {
+  return rawSpeed.toInt();
+}
+
+void serialEvent() {
+    String message = Serial.readStringUntil('\n');
+    int pos = 0;
+    byte parameterPos = 0;
+    String token;
+    String commandParams[NUM_PARAMS];
+    while (parameterPos <= NUM_PARAMS && (pos = message.indexOf(' ')) != -1 || message.length() > 0) {
+        if(pos != -1) {
+          commandParams[parameterPos] = message.substring(0, pos);
+          message.remove(0, pos + 1);
+        } else {
+          commandParams[parameterPos] = message;
+          message = "";
+        }
+        parameterPos++;
+    }
+
+    if(parameterPos != NUM_PARAMS) {
+      Serial.println("Invalid command: Use 'motor direction speed'");
+    } else {
+      Direction direction;
+      Motor *motor;
+      byte speed;
+      for(byte i = 0; i < NUM_PARAMS; i++) {
+        String param = commandParams[i];
+        switch(i) {
+          case 0:
+            motor = readMotor(param);
+            break;
+          case 1:
+            direction = readDirection(param);
+            break;
+          case 2:
+            speed = readSpeed(param);
+            break;
+        }
+      }
+      Serial.println("Updating motors.");
+      motor->setPowerRatio(speed, direction);     
+    }
+    lastCommandTime = millis();
+}
 
 void loop() {
   digitalWrite(STATUSLED, ((millis() / 1000 % 2) == 0 ? HIGH : LOW));
 
-  if(Serial && Serial.available() >= 3) {
-      char motorId = Serial.read();
-      
-      Direction direction;
-       switch(Serial.read()) {
-        case 'S':
-          direction = STOP;
-          break;
-        case 'F':
-          direction = FORWARD;
-          break;
-        case 'B':
-          direction = BACKWARD;
-          break;
-        default:
-          direction = STOP;
-      }
-      
-      unsigned char speed = Serial.read();
-      Motor *motor; 
-      if(motorId == 'R') {
-        motor = rightMotor;
-      } else {
-        motor = leftMotor;
-      }
-      Serial.println("Updating motors.");
-      motor->setPowerRatio(speed, direction);
-      lastCommandTime = millis();
-  } else if(millis() - lastCommandTime > 1500) {
+  if(millis() - lastCommandTime > 1500) {
     if(lastIdle != lastCommandTime) {
-      Serial.println("Idling motors.");
+      if(Serial) {
+        Serial.println("Idling motors.");
+      }
+      
       leftMotor->stop();
       rightMotor->stop();
       lastIdle = lastCommandTime;
@@ -152,4 +194,3 @@ void loop() {
 
   // TODO implement auto off if no recent command has been given
 // TODO set the PWM clock speed for 20KHz
-
